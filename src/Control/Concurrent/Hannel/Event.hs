f068be0a99c4beb9158d0f9b4eb29850557b056e
YellPika <yellpika@gmail.com>
2013-09-16 21:55:20 -0400

    Added tee and split.

--- src/Control/Concurrent/Hannel/Event.hs
+++ src/Control/Concurrent/Hannel/Event.hs
@@ -1,9 +1,12 @@
 module Control.Concurrent.Hannel.Event (
-    Event (), sync, merge, wrap, post, threadID
+    Event (), sync, merge, tee, split, wrap, post, threadID
 ) where
 
+import Control.Applicative ((<|>))
 import Control.Monad (msum)
+import Control.Concurrent.Hannel.Internal.Channel (signal, swap)
 import Control.Concurrent.Hannel.Internal.Event (Event (), sync, wrap, post, threadID)
+import Control.Concurrent.Hannel.Internal.SwapChannel (create, other)
 
 -- |Merges a list of events. The resulting event will wait for all the source
 -- events to synchronize before returning a value. Unlike
@@ -22,3 +25,27 @@ merge xs = do
 splits :: [a] -> [([a], a, [a])]
 splits [] = []
 splits (x:xs) = ([], x, xs) : map (\(l, c, r) -> (x:l, c, r)) (splits xs)
+
+-- |Splits an event into two events that will be notified
+-- simultaneously when the original event is fired.
+tee :: Event a -> Event (Event a, Event a)
+tee event = do
+    channel <- create
+
+    let client = signal $ other channel
+        server = do
+            x <- event
+            swap channel x
+            return x
+        output = client <|> server
+    
+    return (output, output)
+
+-- |Splits an event into a user defined number of events that will be
+-- notified simultaneously when the original event is fired.
+split :: Int -> Event a -> Event [Event a]
+split 0 _ = return []
+split count event = do
+    (x, y) <- tee event
+    xs <- split (count - 1) y
+    return (x:xs)
