d3ab76e35535762d5fa2120d73906657285b3698
YellPika <yellpika@gmail.com>
2013-09-21 12:18:43 -0400

    Grah, forgot a '|', grah!

--- src/Control/Concurrent/Event/Class.hs
+++ src/Control/Concurrent/Event/Class.hs
@@ -9,7 +9,7 @@ import Control.Concurrent.Event.Base (Event)
 import Control.Monad (MonadPlus)
 import Control.Monad.Trans (MonadTrans, lift)
 
--- Describes a computation based on an event.
+-- |Describes a computation based on an event.
 class MonadPlus m => MonadEvent m where
     -- |Lifts an event into a computation.
     liftEvent :: Event a -> m a
