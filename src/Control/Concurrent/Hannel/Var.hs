8c8c7107eabdc948e22a867080d94fdfd9c6d006
YellPika <yellpika@gmail.com>
2013-09-23 13:54:52 -0400

    Added Var

---
+++ src/Control/Concurrent/Hannel/Var.hs
@@ -0,0 +1,9 @@
+module Control.Concurrent.Hannel.Var (
+    module Control.Concurrent.Hannel.Var.Class,
+    module Control.Concurrent.Hannel.Var.Mutable,
+    module Control.Concurrent.Hannel.Var.Immutable
+) where
+
+import Control.Concurrent.Hannel.Var.Class
+import Control.Concurrent.Hannel.Var.Mutable
+import Control.Concurrent.Hannel.Var.Immutable
