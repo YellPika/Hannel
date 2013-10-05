ea4549a78f260c50805ac80e5b818a6d5fcfaf9a
YellPika <yellpika@gmail.com>
2013-10-05 11:24:41 -0400

    Cleaned up SyncLock.

--- src/Control/Concurrent/Hannel/Event/SyncLock.hs
+++ src/Control/Concurrent/Hannel/Event/SyncLock.hs
@@ -6,25 +6,30 @@ module Control.Concurrent.Hannel.Event.SyncLock (
 ) where
 
 import Control.Concurrent.MVar (MVar, newMVar, putMVar, takeMVar)
+import Control.Monad (when)
 import Data.IORef (IORef, newIORef, readIORef, atomicWriteIORef)
 import Data.List (sort)
 import Data.Unique (Unique, newUnique)
 
+-- A SyncLock is a lock-like data structure that may be in one of three states:
+-- * Locked - the lock is in use, and will block other attempts to obtain it.
+-- * Unlocked - the lock is not in use, and may be obtained by anyone.
+-- * Synced - the lock has been permanently locked.
 data SyncLock = SyncLock {
     identifier :: Unique,
-    handle :: SyncLockHandle
-}
-
-data SyncLockHandle = SyncLockHandle {
     status :: MVar Bool,
     synced :: IORef Bool
 }
 
 instance Eq SyncLock where
     x == y = identifier x == identifier y
+
+-- SyncLocks are ordered, which allows an arbitrary number of
+-- SyncLocks to be obtained simultaneously without risking deadlock.
 instance Ord SyncLock where
     compare x y = compare (identifier x) (identifier y)
 
+-- A new SyncLock is unlocked by default.
 newSyncLock :: IO SyncLock
 newSyncLock = do
     identifier' <- newUnique
@@ -33,15 +38,16 @@ newSyncLock = do
 
     return SyncLock {
         identifier = identifier',
-        handle = SyncLockHandle {
-            status = status',
-            synced = synced'
-        }
+        status = status',
+        synced = synced'
     }
 
 isSynced :: SyncLock -> IO Bool
-isSynced = readIORef . synced . handle
+isSynced = readIORef . synced
 
+-- Obtains a set of locks in a deadlock free manner. If none of the locks have
+-- already been synced, then a specified action is executed, and all the locks
+-- become synced.
 withAll :: [SyncLock] -> IO a -> IO (Maybe a)
 withAll locks action = withAll' [] $ sort locks
   where
@@ -50,32 +56,28 @@ withAll locks action = withAll' [] $ sort locks
         mapM_ sync obtained
         return $ Just output
     withAll' obtained (x:xs) = do
-        acquisition <- acquire x
-        case acquisition of
-            Just handle' -> withAll' (handle' : obtained) xs
-            Nothing -> do
-                mapM_ release obtained
-                return Nothing
-
-acquire :: SyncLock -> IO (Maybe SyncLockHandle)
-acquire lock = do
-    let handle' = handle lock
-
-    synced' <- readIORef $ synced handle'
-    if synced' then
-        return Nothing
-    else do
-        synced'' <- takeMVar $ status handle'
-        if synced'' then do
-            putMVar (status handle') True
+        acquired <- acquire x
+        if acquired then
+            withAll' (x : obtained) xs
+        else do
+            mapM_ release obtained
             return Nothing
-        else
-            return $ Just handle'
 
-release :: SyncLockHandle -> IO ()
-release handle' = putMVar (status  handle') False
+acquire :: SyncLock -> IO Bool
+acquire lock = do
+    synced' <- isSynced lock
 
-sync :: SyncLockHandle -> IO ()
-sync handle' = do
-    atomicWriteIORef (synced handle') True
-    putMVar (status handle') True
+    if synced' then
+        return False
+    else do
+        synced'' <- takeMVar $ status lock
+        when synced'' $ putMVar (status lock) True
+        return $ not synced''
+
+release :: SyncLock -> IO ()
+release lock = putMVar (status lock) False
+
+sync :: SyncLock -> IO ()
+sync lock = do
+    atomicWriteIORef (synced lock) True
+    putMVar (status lock) True
