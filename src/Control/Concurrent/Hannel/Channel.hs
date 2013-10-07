8e40190f7e3c4cbe51adb8965e2ac1890bc2fc08
YellPika <yellpika@gmail.com>
2013-10-06 21:14:50 -0400

    Implemented broadcast channels.

--- src/Control/Concurrent/Hannel/Channel.hs
+++ src/Control/Concurrent/Hannel/Channel.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE Safe #-}
 
 module Control.Concurrent.Hannel.Channel (
+    module Control.Concurrent.Hannel.Channel.Broadcast,
     module Control.Concurrent.Hannel.Channel.Round,
     module Control.Concurrent.Hannel.Channel.Swap
 ) where
 
+import Control.Concurrent.Hannel.Channel.Broadcast
 import Control.Concurrent.Hannel.Channel.Round
 import Control.Concurrent.Hannel.Channel.Swap
---
+++ src/Control/Concurrent/Hannel/Channel/Broadcast.hs
@@ -0,0 +1,65 @@
+module Control.Concurrent.Hannel.Channel.Broadcast (
+    BroadcastChannel (), newBroadcastChannel, broadcast, enroll,
+    BroadcastListener (), listen
+) where
+
+import Control.Concurrent.Hannel.Event
+import Control.Concurrent.Hannel.EventHandle
+
+import Control.Applicative ((<$>), (<$), (<|>))
+import Control.Arrow (second)
+import Control.Monad (forM, msum)
+
+data BroadcastChannel i o = Broadcast {
+    broadcast :: i -> Event [o],
+    enroll :: Event (BroadcastListener i o)
+}
+
+data BroadcastListener i o = Listener {
+    listen :: o -> Event i
+}
+
+data BroadcastConnection i o = Connection {
+    sendConnection :: i -> Event o,
+    closeConnection :: Event ()
+}
+
+newBroadcastChannel :: Event (BroadcastChannel i o)
+newBroadcastChannel = do
+    (createConnection, requestConnection) <- swap
+    (requestBroadcast, receiveBroadcast) <- swap
+
+    let send xs = do
+            (input, respond) <- receiveBroadcast ()
+            output <- forM xs $ flip sendConnection input
+            respond output
+            return xs
+        add xs = (: xs) <$> requestConnection ()
+        close = msum . map (\(x, xs) -> xs <$ closeConnection x) . deletions
+
+    (_, handle) <- forkServer [] $ \xs ->
+        send xs <|> add xs <|> close xs
+
+    return Broadcast {
+        broadcast = \value -> wrapEvent handle $ do
+            (respond, wait) <- swap
+            requestBroadcast (value, respond)
+            wait (),
+
+        enroll = wrapEvent handle $ do
+            (sendListener, receiveListener) <- swap
+            (listenerHandle, closeListener) <- newEventHandle
+
+            createConnection Connection {
+                sendConnection = sendListener,
+                closeConnection = closeListener
+            }
+
+            return Listener {
+                listen = wrapEvent listenerHandle . wrapEvent handle . receiveListener
+            }
+    }
+
+deletions :: [a] -> [(a, [a])]
+deletions [] = []
+deletions (x:xs) = (x, xs) : map (second (x :)) (deletions xs)
